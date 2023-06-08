a2f46f386f4315b93fdf4e98275c0091a0552a8a
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-06-08 16:26:22 +0200

    build: add generateHeaders, buildDebug and buildRelease tasks

--- native/build.gradle.kts
+++ native/build.gradle.kts
@@ -1,17 +1,30 @@
-tasks.register("printProjectRoot") {
-    doLast {
-        println(rootProject.projectDir)
-    }
+import java.io.FileOutputStream
+import java.io.OutputStream
+
+tasks.register("printPath") {
+    println("${rootProject.projectDir}/core/src/main/resources/scala-native")
 }
 
 tasks.register("generateHeaders") {
+    val outputFile = File("${rootProject.projectDir}/core/src/main/resources/scala-native/operations.h")
+    val outputStream: OutputStream = FileOutputStream(outputFile)
+    project.exec {
+        standardOutput = outputStream
+        commandLine("cbindgen")
+    }
 }
 
 tasks.register("test") {
 }
 
 tasks.register("buildDebug") {
+    project.exec {
+        commandLine("cargo", "build")
+    }
 }
 
 tasks.register("buildRelease") {
+    project.exec {
+        commandLine("cargo", "build", "--release")
+    }
 }
