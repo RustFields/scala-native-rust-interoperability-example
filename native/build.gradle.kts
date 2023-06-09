7a426012a9c46088b72b97c328c76d4c9922f40f
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-06-09 12:01:04 +0200

    build: remove unused gradle task

--- native/build.gradle.kts
+++ native/build.gradle.kts
@@ -1,10 +1,6 @@
 import java.io.FileOutputStream
 import java.io.OutputStream
 
-tasks.register("printPath") {
-    println("${rootProject.projectDir}/core/src/main/resources/scala-native")
-}
-
 tasks.register("generateHeaders") {
     val outputFile = File("${rootProject.projectDir}/core/src/main/resources/scala-native/operations.h")
     val outputStream: OutputStream = FileOutputStream(outputFile)
